127282672cd915f1a6ea24f264d671bd1cd1563d
Techflash <michaelfgarofalo@gmail.com>
2022-04-03 21:38:28 -0700

    Fix # of increments of vga_buffer
    Make the vga_buffer variable increment the correct number of times

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -4,7 +4,7 @@ extern "C" {
 	extern void printTest();
 }
 void kernelMain() {
-	for (uint_fast8_t i = 0; i <= 7; i++) {
+	for (uint_fast8_t i = 0; i <= 8; i++) {
 		*vga_buffer++; // Increment the VGA buffer a little as to not overwrite the "Booting.." message
 	}
 	print((const uint_fast8_t*)".");
