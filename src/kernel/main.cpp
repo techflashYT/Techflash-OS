9bdabecdc173ab10bbd483eaad4ac537add5967c
Techflash <michaelfgarofalo@gmail.com>
2022-04-04 00:06:36 -0700

    Print a prompt and put cursor in the right position for it.

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -6,6 +6,9 @@ extern "C" {
 void kernelMain() {
 	print((const uint_fast8_t*)".", 9, 0);
 	print((const uint_fast8_t*)"Kernel Loaded", 0, 1, GREEN);
+	print((const uint_fast8_t*)"> ", 0, 2); // Print a prompt
+	terminal.cursor.enable(15, 15);
+	terminal.cursor.position.change(1, 2);
 	halt();
 	return;
 }
