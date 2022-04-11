e9186dd7f02f82035465842ba62ea5983f47bca3
techflashYT-alt <101209944+techflashYT-alt@users.noreply.github.com>
2022-04-11 11:56:01 -0700

    Fix broken syntax (low screen brightness)

--- src/cstdlib/print/putc.cpp
+++ src/cstdlib/print/putc.cpp
@@ -1,6 +1,7 @@
-void putc(const char *c, void *stream, uint8_t x = 255, uint8_t y = 255)
+void putc(const char *c, void *stream, uint8_t x = 255, uint8_t y = 255) {
 	if (x == 255 && y == 255) {
 		x = terminal.cursor.position.x;
 		y = terminal.cursor.position.y;
 	}
 	vga_buffer[((x == 255) ? terminal.position.x : x) + (((y == 255) ? terminal.position.y : y) * VGA_WIDTH)] = c;
+}
