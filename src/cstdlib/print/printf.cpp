f2a4da313c1395fb288a7e2e72e2dd08e06f9df7
techflashYT-alt <101209944+techflashYT-alt@users.noreply.github.com>
2022-04-11 11:58:24 -0700

    A

--- src/cstdlib/print/printf.cpp
+++ src/cstdlib/print/printf.cpp
@@ -2,29 +2,29 @@
 #include <stdarg.h>
 void fputs(const char *str, void *stream);
 void putchar(char c, uint8_t x = 255, uint8_t y = 255);
-extern "C" {
-	void printf(const char *format, ...) {
-		va_list ap;
-		va_start(ap, format);
-
-		const char *ptr;
-		for (ptr = format; *ptr != '\0'; ptr++) {
-			if (*ptr == '%') {
-				ptr++;
-				switch (*ptr) {
-					case 's':
-						fputs(va_arg(ap, const char*), stdout);
-						break;
-					case '%':
-						putchar('%');
-						break;
-					}
+uint8_t printf(const char *format, ...) {
+	va_list ap;
+	va_start(ap, format);
+	uint8_t i = 0;
+	const char *ptr;
+	for (ptr = format; *ptr != '\0'; ptr++) {
+		if (*ptr == '%') {
+			ptr++;
+			switch (*ptr) {
+				case 's':
+					i += fputs(va_arg(ap, const char*), stdout);
+					break;
+				case '%':
+					putchar('%');
+					i++
+					break;
 				}
-			else {
-				putchar(*ptr, 9, 0);
 			}
+		else {
+			putchar(*ptr, 9, 0);
+			i++;
 		}
-		va_end(ap);
-		return;
 	}
+	va_end(ap);
+	return;
 }
