59a4f884393184aae5dc4bec043df7cefce9edf5
Techflash <michaelfgarofalo@gmail.com>
2022-04-20 21:34:31 -0700

    Fix syntax error + new color

--- message.sh
+++ message.sh
@@ -13,7 +13,7 @@ then
     printf "\033[1;32mSTEP 4/4: Linked!\033[0m\r\n"
 elif [ "$1" = "5" ]
 then
-    printf "\033[1;32mFully compiled in bin/bootsect.bin!\033[0m\r\n"
+    printf "\033[1;36mFully compiled in bin/bootsect.bin!\033[0m\r\n"
 elif [ "$1" = "6" ]
 then
     printf "\033[1;33mCompiling C Standard library to a static library.\r\nThis more than likely will fail.\033[0m\r\n"
@@ -21,6 +21,7 @@ elif [ "$1" = "e" ]
 then
     printf "\033[1;31m"
 elif [ "$1" = "r" ]
+then
     tput sgr0
 else
     printf "\033[1;31mERROR: Invalid argument!\033[0m\r\n"
