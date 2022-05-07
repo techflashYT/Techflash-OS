b48e3c3e72e9b1d7160ce789c7eb4df6ce10b1a9
Your Name <you@example.com>
2022-05-07 14:02:05 -0700

    And nuke everything for print to make it good

--- src/cstdlib/print/putc.cpp
+++
@@ -1,9 +0,0 @@
-#include "../util.h"
-#include "../../shared.hpp"
-extern "C" {
-	void putc(const char c, void *stream);
-}
-void putc(const char c, void *stream) {
-	(void)stream;
-	vga_buffer[terminal.cursor.position.x + terminal.cursor.position.y * VGA_WIDTH] = c;
-}
--- src/cstdlib/print/putchar.c
+++
@@ -1,6 +0,0 @@
-#include "../util.h"
-void putc(const char c, void *stream);
-void putchar(char c) {
-	// Literally just calls putc with the stdout steam
-	putc(c, stdout);
-}
