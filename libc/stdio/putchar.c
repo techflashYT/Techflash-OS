35b8a977fe007fc7ddd6dc18f462aee465b3af0b
Your Name <you@example.com>
2022-05-09 00:13:56 -0700

    Fixes

--- libc/stdio/putchar.c
+++ libc/stdio/putchar.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 #if defined(__is_libk)
 #include <kernel/tty.h>
 #endif
-
+bool checkIfNextCharIsNewline = false;
 int putchar(int ic) {
 #if defined(__is_libk)
 	char c = (char) ic;
+	if (c == '\r') {
+		checkIfNextCharIsNewline = true;
+		return ic;
+	}
+	if (c == '\n' && checkIfNextCharIsNewline) {
+		terminal_write(&c, sizeof(c));
+		checkIfNextCharIsNewline = false;
+	}
+	if (c != '\n' && checkIfNextCharIsNewline) {
+		terminal_write(&c, sizeof(c));
+	}
+		
 	terminal_write(&c, sizeof(c));
 #else
 	// TODO: Implement stdio and the write system call.
