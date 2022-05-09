b1ff93057214c30e2c25075026528656c856a618
Your Name <you@example.com>
2022-05-08 22:33:07 -0700

    Tell GCC that those conversions are fine

--- kernel/arch/i386/vga.h
+++ kernel/arch/i386/vga.h
@@ -23,7 +23,7 @@ enum vga_color {
 };
 
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
-	return fg | bg << 4;
+	return (uint8_t)(fg | bg << 4);
 }
 
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
