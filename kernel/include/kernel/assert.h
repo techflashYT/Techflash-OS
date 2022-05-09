d5d85118af0617a6f691b9a3a0d42fa7da25adb9
Your Name <you@example.com>
2022-05-09 00:49:45 -0700

    Try it with pragma once

--- kernel/include/kernel/assert.h
+++ kernel/include/kernel/assert.h
@@ -1,7 +1,5 @@
-#ifndef ASSERT_H
-#define ASSERT_H
+#pragma once
 
 void assert(const char *message, const char *file, int line) {
 	printf("ASSERT: File: %s, Line: %d, Message:\r\n %s\r\n", file, line, message);
 }
-#endif
