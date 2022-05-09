8d12847116f5c354904f239feb6729f2bde2b8bc
Your Name <you@example.com>
2022-05-09 00:49:07 -0700

    Make it only include once

--- kernel/include/kernel/assert.h
+++ kernel/include/kernel/assert.h
@@ -1,3 +1,7 @@
+#ifndef ASSERT_H
+#define ASSERT_H
+
 void assert(const char *message, const char *file, int line) {
 	printf("ASSERT: File: %s, Line: %d, Message:\r\n %s\r\n", file, line, message);
 }
+#endif
