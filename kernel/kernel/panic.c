977a6cee6b06d6953ece4d18f77b0daf888f9d67
Your Name <you@example.com>
2022-05-08 22:52:18 -0700

    Add kernel panic

---
+++ kernel/kernel/panic.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+void panic(const char* message) {
+	printf("Kernel panic: %s\r\n", message);
+	while (true) {
+		asm volatile("cli");
+		asm volatile("hlt");
+	}
+}
--- kernel/kernel/stack.c
+++ kernel/kernel/stack.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include <kernel/panic.h>
+
 #if (UINT32_MAX == UINTPTR_MAX)
 #define STACK_CHK_GUARD 0xe2dee396
 #else
@@ -9,7 +11,7 @@
 
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
 
-__attribute__((noreturn)) void __stack_chk_fail(void) {
+void __stack_chk_fail(void) {
 #if __STDC_HOSTED__
 	abort();
 #elif __is_kernel
