7e03f555985dd9d9a00d469a780ef5341599dc4b
Your Name <you@example.com>
2022-05-09 00:54:25 -0700

    Add a todo

--- kernel/kernel/assert.c
+++ kernel/kernel/assert.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
 void assert(const char *message, const char *file, int line) {
 	printf("ASSERT: File: %s, Line: %d, Message:\r\n %s\r\n", file, line, message);
+	// TODO: Fix message not working right
 }
