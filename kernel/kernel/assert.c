13b8788adc9625e00a4c767748ed167ffe297027
Your Name <you@example.com>
2022-05-09 18:21:28 -0700

    See if putting Message first with fix it

--- kernel/kernel/assert.c
+++ kernel/kernel/assert.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 void assert(const char *message, const char *file, int line) {
-	printf("ASSERT: File: %s, Line: %d, Message:\r\n%s\r\n", file, line, message);
+	printf("ASSERT: Message: %s, File: %s, Line: %d\r\n", message, file, line);
 	// TODO: Fix message not working right
 }
