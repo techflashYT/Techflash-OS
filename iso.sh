40b62398ea3fecdb4e2c1eae7451f6804a42b102
techflashYT <michaelfgarofalo@gmail.com>
2022-05-24 21:43:08 +0000

    ISO config

--- iso.sh
+++ iso.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
-. ./build.sh
+# Check if sysroot/boot/tfos.elf exists
+if [ ! -f sysroot/boot/tfos.elf ]; then
+	. ./build.sh
+fi
 
 
 #cp sysroot/boot/tfos.elf isodir/boot/install
@@ -8,9 +11,7 @@ cp -r sysroot isodir
 mv isodir/boot/tfos.elf isodir/boot/install
 mkdir -p isodir/boot/grub
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "Install Techflash OS" {
-	multiboot /boot/install
-}
+$(cat ISO_GRUB_CFG.cfg)
 EOF
 grub-mkrescue -o bin/TFOS_ISO.iso isodir
 rm -r isodir
