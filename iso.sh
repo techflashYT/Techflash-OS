ff352d49dd4c3b435f48932869f4b6304bfc995f
techflashYT <michaelfgarofalo@gmail.com>
2022-05-28 21:50:25 +0000

    suppress output of grub-mkrescue
    Pipe the output of the 'grub-mkrescue' command to /dev/null

--- iso.sh
+++ iso.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 # Check if sysroot/boot/tfos.elf exists
 if [ ! -f sysroot/boot/tfos.elf ]; then
@@ -13,5 +13,5 @@ mkdir -p isodir/boot/grub
 cat > isodir/boot/grub/grub.cfg << EOF
 $(cat ISO_GRUB_CFG.cfg)
 EOF
-grub-mkrescue -o bin/TFOS_ISO.iso isodir
+grub-mkrescue -o bin/TFOS_ISO.iso isodir &> /dev/null
 rm -r isodir
