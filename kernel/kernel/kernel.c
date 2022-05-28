cf4e69abf7bed0893776dba2d4d351c30f4fc2e0
techflashYT <michaelfgarofalo@gmail.com>
2022-05-28 16:20:02 +0000

    Grub arguments!
    Use a bit of asm to get grub arguments working, and a thing for testing if handwritten asm will run on bootup

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-#include <assert.h>
 
 #include <kernel/tty.h>
 #include <kernel/sourceFileInfo.h>
 #include <kernel/timer.h>
 #include <kernel/GDT.h>
 #include <kernel/debug.h>
+#include <kernel/panic.h>
+
+extern int _testasm();
 static sourceFileInfo fileInfo = {
 	.fileName       = "kernel/kernel.c",
 	.lastEditor     = "Techflash",
@@ -15,16 +17,26 @@ static sourceFileInfo fileInfo = {
 	.versionMinor   = 0,
 	.versionPatch   = 2
 };
-void kernelMain(char* GrubArgs) {
-	// Retrieve the arguments from GRUB that were passed in from the assembly earlier.
-	// TODO: This ^
+void kernelMain(void* grubCMDline) {
+	
+
 	terminalInit();
 	printf("[ %d.%d ] Terminal initialized\r\n", timer.now.seconds(), timer.now.milliseconds());
 	GDTinit();
 	printf("[ %d.%d ] Global Descriptor Table initialized\r\n", timer.now.seconds(), timer.now.milliseconds());
-	printf("[ %d.%d ] KERN_ARGS: %s\r\n", timer.now.seconds(), timer.now.milliseconds(), GrubArgs);
+	printf("[ %d.%d ] Test that calling a handwritten ASM function works: ", timer.now.seconds(), timer.now.milliseconds());
+	
+	if (_testasm() != 0x12345678) {
+		printf("FAILED\r\n");
+		panic("Handwritten ASM test returned a non-136 value");
+	}
+	else {
+		printf("PASSED\r\n");
+	}
+	printf("[ %d.%d ] KERN_ARGS: %s\r\n", timer.now.seconds(), timer.now.milliseconds(), grubCMDline);
+	// TODO: Handle arguments here
 	printf("Welcome to Techflash OS v%d.%d.%d!\r\n", fileInfo.versionMajor, fileInfo.versionMinor, fileInfo.versionPatch);
-	printf("> ");
+	printf("> \r\n");
 	kernelDebug((const char*)"The kernel has reached the end of execution.  The system will now halt.", fileInfo, __LINE__);
 	for (;;) {
 		
