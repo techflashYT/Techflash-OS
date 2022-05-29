a76f6d51fa9bbdc41e1ad44ddda94b2e26bd3608
techflashYT <michaelfgarofalo@gmail.com>
2022-05-29 13:59:59 +0000

    Make the scripts a little nicer

--- all.sh
+++ all.sh
@@ -3,10 +3,19 @@ if [ "$1" != "--no-pull" ]
 then
 	git pull
 fi
+if ! command -v i686-elf-gcc > /dev/null || ! command -v i686-elf-as > /dev/null || ! command -v i686-elf-ld > /dev/null
+then
+	echo "Uh oh!  Looks like you never installed the i686-elf-gcc cross compiler!  Please follow the steps in the wiki to install it."
+	exit 1
+fi
 clear
 ./clean.sh
-clear
-./build.sh
+if ! ./build.sh
+then
+	sleep 0.05
+	echo "Uh oh!  Looks like something failed to build!  Please check the above output to see what went wrong."
+	exit 2
+fi
 echo "ISO"
 ./iso.sh
 echo "Done!  Boot iso is in bin/TFOS_ISO.iso"
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 . ./util/headers.sh
 
 for PROJECT in $PROJECTS; do
-  (cd "$PROJECT" && DESTDIR="$SYSROOT" $MAKE install)
+  (cd "$PROJECT" && DESTDIR="$SYSROOT" $MAKE install 2> >(../util/rederr.sh >&2))
 done
