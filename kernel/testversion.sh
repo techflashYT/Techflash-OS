79b21d99eb0fec9ac78c0f5645a7c9c9400dd1e5
techflashYT <michaelfgarofalo@gmail.com>
2022-05-29 14:01:34 +0000

    script for verifying the version number

---
+++ kernel/testversion.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ "$1" = "" ]
+then
+	echo "This script is not meant to be run standalone!  This is a helper script for the Makefile!"
+	exit 1
+fi
+
+# Seperate the args into variables
+CONFIGURE_KERN_VERSION_MAJOR=$1
+CONFIGURE_KERN_VERSION_MINOR=$2
+CONFIGURE_KERN_VERSION_PATCH=$3
+EXPECTED_KERN_VERSION_MAJOR=$4
+EXPECTED_KERN_VERSION_MINOR=$5
+EXPECTED_KERN_VERSION_PATCH=$6
+
+if [ "$CONFIGURE_KERN_VERSION_MAJOR" = "$EXPECTED_KERN_VERSION_MAJOR" ] && [ "$CONFIGURE_KERN_VERSION_MINOR" = "$EXPECTED_KERN_VERSION_MINOR" ] && [ "$CONFIGURE_KERN_VERSION_PATCH" = "$EXPECTED_KERN_VERSION_PATCH" ]
+then
+	exit 0
+else
+	exit 1
+fi
+
