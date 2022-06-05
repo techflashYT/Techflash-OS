34e7c7d4c03ca52a2431ea81b710d188e1d45a63
techflashYT <michaelfgarofalo@gmail.com>
2022-06-05 16:16:41 +0000

    Stuff for the GRUB menu
    Background designed for 4:3 resolutions, font, and framebuffer term in GRUB.

--- iso.sh
+++ iso.sh
@@ -10,8 +10,11 @@ fi
 cp -r sysroot isodir
 mv isodir/boot/tfos.elf isodir/boot/install
 mkdir -p isodir/boot/grub
+mkdir -p isodir/boot/grub/{fonts,themes,locale}
 cat > isodir/boot/grub/grub.cfg << EOF
 $(cat ISO_GRUB_CFG.cfg)
 EOF
+cp grub_bg.png isodir/boot/bg.png
+cp font.pf2 isodir/boot/grub/fonts/unicode.pf2
 grub-mkrescue -o bin/TFOS_ISO.iso isodir &> /dev/null
 rm -r isodir
