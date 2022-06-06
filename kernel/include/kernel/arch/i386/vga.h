114748f26dbdbdffbd0d6f83f2cdc5fcab113c3a
techflashYT <michaelfgarofalo@gmail.com>
2022-06-06 12:50:57 +0000

    Forgot to add the new vga.h before, so here it is

---
+++ kernel/include/kernel/arch/i386/vga.h
@@ -0,0 +1,29 @@
+#ifndef ARCH_I386_VGA_H
+#define ARCH_I386_VGA_H
+
+#include <stdint.h>
+
+enum vgaColor {
+	VGA_COLOR_BLACK = 0,
+	VGA_COLOR_BLUE = 1,
+	VGA_COLOR_GREEN = 2,
+	VGA_COLOR_CYAN = 3,
+	VGA_COLOR_RED = 4,
+	VGA_COLOR_MAGENTA = 5,
+	VGA_COLOR_BROWN = 6,
+	VGA_COLOR_LIGHT_GRAY = 7,
+	VGA_COLOR_DARK_GRAY = 8,
+	VGA_COLOR_LIGHT_BLUE = 9,
+	VGA_COLOR_LIGHT_GREEN = 10,
+	VGA_COLOR_LIGHT_CYAN = 11,
+	VGA_COLOR_LIGHT_RED = 12,
+	VGA_COLOR_LIGHT_MAGENTA = 13,
+	VGA_COLOR_LIGHT_BROWN = 14,
+	VGA_COLOR_WHITE = 15,
+};
+
+uint8_t vgaEntryColor(enum vgaColor fg, enum vgaColor bg);
+uint16_t vgaEntry(unsigned char uc, uint8_t color);
+size_t vgaIndex(size_t x, size_t y);
+
+#endif
