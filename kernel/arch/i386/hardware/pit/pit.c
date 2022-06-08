d898ba7f06aa822018cd6e3aa5a7f23a2c06e550
techflashYT <michaelfgarofalo@gmail.com>
2022-06-08 13:54:26 +0000

    Begin work on the PIT

---
+++ kernel/arch/i386/hardware/pit/pit.c
@@ -0,0 +1,9 @@
+#include <kernel/arch/i386/io.h>
+#include <stdint.h>
+void activateChannel(uint8_t channel, uint8_t frequency) {
+	unsigned int divider = 1193180 / frequency;
+
+	outb(0x43, channel == 0 ? 0x36 : 0xB6);
+	outb(0x40 + channel, (uint8_t)(divider & 0xFF));
+	outb(0x40 + channel, (uint8_t)((divider >> 8) & 0xFF));
+}
