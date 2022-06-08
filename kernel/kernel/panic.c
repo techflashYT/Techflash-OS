db2e5206e074de3a6a35774ef6b44eecbb81bf6f
techflashYT <michaelfgarofalo@gmail.com>
2022-06-08 14:10:02 +0000

    (kind of) Beep the PC Speaker on kernel panic
    Beep the PC Speaker on kernel panic, but not long enough for it to be audible, since I don't have the PIT to know what "time" is on there.

--- kernel/kernel/panic.c
+++ kernel/kernel/panic.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <kernel/arch/i386/pcspeaker.h>
 void panic(const char* message) {
+	playSound(1000);
 	printf("Kernel panic: %s\r\n", message);
+	// TODO: Use Programmable Interval timer to wait about 2 seconds
+	noSound();
 	while (true) {
 		asm volatile("cli");
 		asm volatile("hlt");
