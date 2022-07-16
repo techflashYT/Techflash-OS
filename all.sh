b8bc1a6cee7acdf110e5d8fa7253aef14b54fdff
techflashYT <michaelfgarofalo@gmail.com>
2022-07-16 11:55:14 -0700

    Better syntax, 🪲fix bug in cross-comp checker, make build directories.

--- all.sh
+++ all.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
-if [ "$1" != "--no-pull" ]
-then
+if [ "$1" != "--no-pull" ]; then
 	git pull
 fi
-if ! command -v i686-elf-gcc > /dev/null || ! command -v i686-elf-as > /dev/null || ! command -v i686-elf-ld > /dev/null || ! command -v x86_64-elf-gcc > /dev/null || ! command -v x86_64-elf-as > /dev/null || ! command -v x86_64-elf-ld > /dev/null 
-then
+if ! ( command -v i686-elf-gcc > /dev/null || ! command -v i686-elf-as > /dev/null || ! command -v i686-elf-ld > /dev/null ) && ( ! command -v x86_64-elf-gcc > /dev/null || ! command -v x86_64-elf-as > /dev/null || ! command -v x86_64-elf-ld > /dev/null ); then
 	echo "Uh oh!  Looks like you never installed the i686-elf-gcc/x86_64-elf-gcc cross compiler!  Please follow the steps in the wiki to install it."
 	exit 1
 fi
-if [ ! -f .config ]
-then
+if [ ! -f .config ]; then
 	echo "Uh oh!  Looks like never ran ./configure!  Please run ./configure and then run this script again."
 	exit 2
 fi
 ./clean.sh
-if ! ./build.sh
-then
+if ! [ -d bin ]; then
+	make -f util/Makefile builddirs
+fi
+if ! ./build.sh; then
 	sleep 0.05
 	echo "Uh oh!  Looks like something failed to build!  Please check the above output to see what went wrong."
 	exit 3
