18a50bf716c600d0baf0ffd7ebdb6d13f6111a45
techflashYT <michaelfgarofalo@gmail.com>
2022-07-22 21:05:02 -0700

    add linker warning & error color

--- util/rederr.sh
+++ util/rederr.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 while read -r line
 do
-	printf "\x1b[1;31m%s%s\r\n" "$line" "$(tput sgr0)"
+	line=$(echo "$line" | sed 's/^.*ld\:/ld:/;s/\.\.\///g') # Remove the absurdly long prefix to the ld binary & remove the '..' from the path the the build directories
+	if [[ "$line" = *"undefined reference to"* ]] || [[ "$line" = *"multiple defintions of"* ]] || [[ "$line" = *"cannot find"* ]] || [[ "$line" = *"collect2: error:"* ]];then
+		printf "\x1b[1;31m%s%s\r\n" "$line" "$(tput sgr0)"
+	elif [[ "$line" = *"warning"* ]];then
+		printf "\x1b[1;35m%s%s\r\n" "$line" "$(tput sgr0)"
+	else
+		printf "$line\r\n"
+	fi
 done < "${1:-/dev/stdin}"
