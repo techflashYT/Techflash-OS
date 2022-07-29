17a23085f99da50e6bdb841349cc15c63f1ec8d4
techflashYT <michaelfgarofalo@gmail.com>
2022-07-28 20:08:44 -0700

    Set screen to red in kernel panic

--- kernel/panic.c
+++ kernel/panic.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <kernel/environment.h>
+#include <kernel/graphics.h>
+#include <kernel/tty.h>
 void panic(const char* message) {
-	asm volatile("xchgw %bx, %bx");
-	printf("KERNEL PANIC: %s\n", message);
-	asm volatile("cli\n" "hlt");
+	int s = bootboot.fb_scanline;
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Warray-bounds"
+	#pragma GCC diagnostic ignored "-Wstringop-overflow"
+	// Set each pixel of the framebuffer to 0xDD0000 [dark red]
+	for (uint32_t y = 0; y < bootboot.fb_height; y++) {
+		for (uint32_t x = 0; x < bootboot.fb_width; x++) {
+			*((uint32_t*)(&fb + s * y + x * 4)) = 0xDD0000;
+		}
+	}
+	#pragma GCC diagnostic pop
+	kernTTY.color = vga.colors.white;
+	printf("KERNEL PANIC: %s\r\n", message);
+	asm volatile (
+		"cli\n"
+		"hlt"
+	);
 }
