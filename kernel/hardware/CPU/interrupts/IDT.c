49daf51766d5d0ef3c4c37d23d538ba489fed0e1
techflashYT <michaelfgarofalo@gmail.com>
2022-08-04 14:34:49 -0700

    Fix busted IDT implimentation.  Still doesn't work

--- kernel/hardware/CPU/interrupts/IDT.c
+++ kernel/hardware/CPU/interrupts/IDT.c
@@ -2,16 +2,16 @@
 #include <kernel/hardware/CPU/IDT.h>
 idtEntry_t idtEntries[256];
 // idtPtr_t   idtPtr;
-
+extern void IDTFlush();
 void IDTInit() {
 	for (uint64_t i = 0; i < 256; i++) {
-		idtEntries[i].reserved = 0;
+		idtEntries[i].reserved   = 0;
 		idtEntries[i].baseLow    = (uint16_t)(((uint64_t)&ISR0 & 0x000000000000FFFF));
 		idtEntries[i].baseMiddle = (uint16_t)(((uint64_t)&ISR0 & 0x00000000FFFF0000) >> 16);
 		idtEntries[i].baseMiddle = (uint32_t)(((uint64_t)&ISR0 & 0xFFFFFFFF00000000) >> 32);
-		idtEntries[i].always0 = 0;
-		idtEntries[i].sel = 0x08;
-		idtEntries[i].flags = 0x8E;
+		idtEntries[i].always0    = 0;
+		idtEntries[i].sel        = 0x08;
+		idtEntries[i].flags      = 0x8E;
 	}
-	
+	IDTFlush();
 }
