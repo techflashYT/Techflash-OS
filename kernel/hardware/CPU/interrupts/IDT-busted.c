e25a8975bf0121e1864a1a516e450981cfc10273
techflashYT <michaelfgarofalo@gmail.com>
2022-08-04 19:56:01 -0700

    Restore old IDT code

---
+++ kernel/hardware/CPU/interrupts/IDT-busted.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include <kernel/hardware/CPU/IDT.h>
+idtEntry_t idtEntries[256];
+// idtPtr_t   idtPtr;
+extern void IDTFlush();
+void IDTInit() {
+	for (uint64_t i = 0; i < 256; i++) {
+		idtEntries[i].reserved   = 0;
+		idtEntries[i].baseLow    = (uint16_t)(((uint64_t)&ISR1 & 0x000000000000FFFF));
+		idtEntries[i].baseMiddle = (uint16_t)(((uint64_t)&ISR1 & 0x00000000FFFF0000) >> 16);
+		idtEntries[i].baseMiddle = (uint32_t)(((uint64_t)&ISR1 & 0xFFFFFFFF00000000) >> 32);
+		idtEntries[i].always0    = 0;
+		idtEntries[i].sel        = 0x08;
+		idtEntries[i].flags      = 0x8E;
+	}
+	IDTFlush();
+}
