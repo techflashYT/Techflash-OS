e0c09ee7284a28931325d8efbbb309b6d095109d
techflashYT <michaelfgarofalo@gmail.com>
2022-08-05 17:24:15 -0700

    Smaller

--- include/kernel/misc.h
+++ include/kernel/misc.h
@@ -5,21 +5,21 @@ asm volatile (\
 	"xchgw %bx, %bx\n"\
 );
 #define DUMPREGS \
-register uint64_t rax asm("rax"); \
-register uint64_t rbx asm("rbx"); \
-register uint64_t rcx asm("rcx"); \
-register uint64_t rdx asm("rdx"); \
-register uint64_t rbp asm("rbp"); \
-register uint64_t rsp asm("rsp"); \
-register uint64_t rsi asm("rsi"); \
-register uint64_t rdi asm("rdi"); \
-registers_t regs = { \
-	.rax = rax, \
-	.rbx = rbx, \
-	.rcx = rcx, \
-	.rdx = rdx, \
-	.rbp = rbp, \
-	.rsp = rsp, \
-	.rsi = rsi, \
+register uint64_t rax asm("rax");\
+register uint64_t rbx asm("rbx");\
+register uint64_t rcx asm("rcx");\
+register uint64_t rdx asm("rdx");\
+register uint64_t rbp asm("rbp");\
+register uint64_t rsp asm("rsp");\
+register uint64_t rsi asm("rsi");\
+register uint64_t rdi asm("rdi");\
+registers_t regs = {\
+	.rax = rax,\
+	.rbx = rbx,\
+	.rcx = rcx,\
+	.rdx = rdx,\
+	.rbp = rbp,\
+	.rsp = rsp,\
+	.rsi = rsi,\
 	.rdi = rdi \
 };
