ab5c0529ffb031742f797e046414b78065959ee3
techflashYT <michaelfgarofalo@gmail.com>
2022-08-05 17:23:34 -0700

    Add memcpy so that GCC can do some optimizations.

--- include/string.h
+++ include/string.h
@@ -3,3 +3,4 @@
 size_t strlen(const char *str);
 void *memset(void *b, int c, size_t len);
 char *strcat(char* restrict destination, const char* restrict source);
+void *memcpy(void* destination, const void* source, size_t num);
---
+++ libc/string/memcpy.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+void *memcpy(void* destination, const void* source, size_t num) {
+	// Convert to char* for easy access
+	char *src  = (char *)source;
+	char *dest = (char *)destination;
+
+	// Copy bytes
+	for (size_t i = 0; i < num; i++) {
+		dest[i] = src[i];
+	}
+}
