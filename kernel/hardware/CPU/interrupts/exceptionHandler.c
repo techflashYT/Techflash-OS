c0ac33ee4c4460211c52ca751637f34595412417
techflashYT <michaelfgarofalo@gmail.com>
2022-08-05 17:16:04 -0700

    Fix warnings of unavoidable unintialized variables.

--- kernel/hardware/CPU/interrupts/exceptionHandler.c
+++ kernel/hardware/CPU/interrupts/exceptionHandler.c
@@ -3,7 +3,10 @@
 #include <kernel/misc.h>
 __attribute__((noreturn)) void exceptionHandler(void);
 void exceptionHandler() {
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wuninitialized"
 	DUMPREGS
+	#pragma GCC diagnostic pop
 	panic("exceptionHandler(): Unhandled exception in kernel code!", regs);
 	while (true) {};
 }
