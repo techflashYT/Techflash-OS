b5b7b395d0f8068fc044473b7f88ef84e77b7312
techflashYT <michaelfgarofalo@gmail.com>
2022-08-18 17:00:28 -0700

    Remove outdated warning

--- libc/custom/padNumTo.c
+++ libc/custom/padNumTo.c
@@ -9,8 +9,6 @@ char* padNumTo(const char *src, uint8_t padding) {
 	if (len >= padding) {
 		return (char *)src;
 	}
-	// Yes I know this is very.  VERY stupid.  But it technically accounts for every possibility of the number of padding.
-	// FIXME: Cppcheck complains that it's undefined behavior, yet GCC doesn't even spit out a warning.  Find out why.
 	for (size_t i = 0; i < padding - len; i++) {
 		dest[i] = '0';
 	}
