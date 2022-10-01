96a6275f1b770abb1c303a63c461f3e7bdbbc619
techflashYT <michaelfgarofalo@gmail.com>
2022-10-01 16:29:17 -0700

    Better qemu script

--- qemu.sh
+++ qemu.sh
@@ -1,1 +1,1 @@
-./qemu-system-x86_64 -smp 2 -m 512M,slots=2,maxmem=1G -vga cirrus -no-reboot -no-shutdown -serial stdio -cdrom bin/TFOS_ISO.iso
+qemu-system-x86_64 -m 512M,slots=2,maxmem=1G -no-reboot -cpu core2duo -monitor stdio -no-shutdown -serial file:/proc/self/fd/1 -cdrom TFOS_ISO.iso
