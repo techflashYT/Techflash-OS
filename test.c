54dba9e74c293a2142750a5421310272053d36fe
Techflash <72118300+techflashYT@users.noreply.github.com>
2022-12-11 18:53:48 -0700

    update the code for testing the elf loader.

--- test.c
+++ test.c
@@ -1,3 +1,33 @@
+/*asm("\
+.global _start\r\n\
+.type _start, @function\r\n\
+_start:\r\n\
+	mov $0x0, %rax\r\n\
+	ret\r\n\
+");
+*/
+struct {
+	unsigned long long int test1;
+	unsigned long long int test2;
+	unsigned long long int test3;
+	unsigned long long int test4;
+	unsigned long long int test5;
+	unsigned long long int test6;
+	unsigned long long int test7;
+	unsigned long long int test8;
+	unsigned long long int test9;
+	unsigned long long int testa;
+} randomVariable;
 int _start() {
+		randomVariable.test1 = 0;
+		randomVariable.test2 = 0;
+		randomVariable.test3 = 0;
+		randomVariable.test4 = 0;
+		randomVariable.test5 = 0;
+		randomVariable.test6 = 0;
+		randomVariable.test7 = 0;
+		randomVariable.test8 = 0;
+		randomVariable.test9 = 0;
+		randomVariable.testa = 0;
 		return 0;
 }
