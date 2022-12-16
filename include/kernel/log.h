fe6c6c5beca9c39a06abe57baf0d5f329ae52f21
Techflash <72118300+techflashYT@users.noreply.github.com>
2022-12-16 08:12:43 -0700

    completely revamp the logging system

--- include/kernel/log.h
+++ include/kernel/log.h
@@ -1,1 +1,4 @@
-void log(const char *module, const char* message);
+#define LOGLEVEL_DEBUG 0
+#define LOGLEVEL_WARN  1
+#define LOGLEVEL_ERROR 2
+void log(const char *module, const char* message, uint8_t logLevel);
--- kernel/logging.c
+++ kernel/logging.c
@@ -3,7 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <kernel/hardware/serial.h>
-void log(const char *module, const char* message) {
+#include <kernel/graphics.h>
+#include <kernel/tty.h>
+#define LOG_TO_SERIAL
+#define LOG_COLOR
+char *logLevelColorsEscCode[] = {
+	"\x1b[0m", // debug
+	"\x1b[0m\x1b[33m", // warn
+	"\x1b[0m\x1b[31m"  // err
+};
+uint32_t logLevelColorsFrameBuf[] = {
+	0x00AAAAAA, // debug
+	0x00FFD866, // warn
+	0x00BB0000  // err
+};
+void log(const char *module, const char* message, uint8_t logLevel) {
+	// colorize using actual ANSI escape sequences for the serial logging
 	char *buffer = malloc(256);
 
 	buffer[0] = '[';
@@ -24,12 +39,48 @@ void log(const char *module, const char* message) {
 	// Results in:
 	// [{module}] {message}{newline}{NULL terminator}
 	// neatly put into the buffer.
-	serial.writeString(SERIAL_PORT_COM1, buffer);
+	#ifdef LOG_TO_SERIAL
+		#ifdef LOG_COLOR
+			serial.writeString(SERIAL_PORT_COM1, "\x1b[0m");
+			serial.writeString(SERIAL_PORT_COM1, "\x1b[33m[\x1b[1;36m");
+			serial.write(SERIAL_PORT_COM1, buffer[1]);
+			serial.write(SERIAL_PORT_COM1, buffer[2]);
+			serial.write(SERIAL_PORT_COM1, buffer[3]);
+			serial.write(SERIAL_PORT_COM1, buffer[4]);
+			serial.write(SERIAL_PORT_COM1, buffer[5]);
+			serial.write(SERIAL_PORT_COM1, buffer[6]);
+			serial.write(SERIAL_PORT_COM1, buffer[7]);
+			serial.write(SERIAL_PORT_COM1, buffer[8]);
+			serial.writeString(SERIAL_PORT_COM1, "\x1b[0m\x1b[33m]");
+			serial.writeString(SERIAL_PORT_COM1, logLevelColorsEscCode[logLevel]);
+			serial.writeString(SERIAL_PORT_COM1, buffer + 10);
+			serial.writeString(SERIAL_PORT_COM1, "\x1b[0m");
+		#else
+			serial.writeString(SERIAL_PORT_COM1, buffer);
+		#endif
+	#endif
 	/* TODO: When logLevel kernel param is implemented, use the below code:
 	if (env.logLevel < LOGLEVEL_WARN) {
 		puts(buffer);
 	}
 	*/
-	puts(buffer);
+
+	#ifdef LOG_COLOR
+		uint32_t origColor = kernTTY.color;
+		kernTTY.color = vga.colors.yellow;
+		putchar('[');
+		kernTTY.color = vga.colors.lcyan;
+		for (uint8_t i = 1; i != 9; i++) {
+			putchar(buffer[i]);
+		}
+		kernTTY.color = vga.colors.yellow;
+		putchar(']');
+		putchar(' ');
+		kernTTY.color = logLevelColorsFrameBuf[logLevel];
+		puts(buffer + 11);
+		kernTTY.color = origColor;
+	#else
+		puts(buffer);
+	#endif
 	free(buffer);
 }
