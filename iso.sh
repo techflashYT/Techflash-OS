76e0a1329a7560a2ac10f024fc3e54537695a8f1
Techflash <72118300+techflashYT@users.noreply.github.com>
2022-12-22 15:27:41 -0700

    Remove old commented code, and change file paths

--- iso.sh
+++ iso.sh
@@ -28,9 +28,11 @@ mkdir -p isodir/boot/grub
 cd isodir/boot/grub
 mkdir fonts themes locale
 cd ../../..
-cat ISO_GRUB_CFG.cfg > isodir/boot/grub/grub.cfg
-cp grub_bg.png isodir/boot/bg.png
-cp font.pf2 isodir/boot/grub/fonts/unicode.pf2
+cd grub
+cat ISO_GRUB_CFG.cfg > ../isodir/boot/grub/grub.cfg
+cp grub_bg.png ../isodir/boot/bg.png
+cp font.pf2 ../isodir/boot/grub/fonts/unicode.pf2
+cd ..
 if ( command -v grub-mkrescue > /dev/null ); then
 	grub-mkrescue /usr/lib/grub/i386-pc --stdio_sync off -o bin/TFOS_ISO.iso isodir
 else
@@ -38,46 +40,3 @@ else
 		grub2-mkrescue /usr/lib/grub/i386-pc --stdio_sync off -o bin/TFOS_ISO.iso isodir
 	fi
 fi
-
-# cp isodir/tfos.img bin/TFOS_ISO.iso
-
-
-
-
-# if ! [ -f util/mkbootimg/mkbootimg ]; then
-# 	echo "Grabbing latest mkbootimg & bootboot.bin"
-# 	wget https://gitlab.com/bztsrc/bootboot/-/raw/master/dist/bootboot.bin?inline=false -q --progress=bar --show-progress -O bootboot.bin
-# 	mv bootboot.bin isodir/boot/bootboot.bin
-# 	cd util
-# 	wget https://gitlab.com/bztsrc/bootboot/-/archive/master/bootboot-master.tar.bz2?path=mkbootimg -q --progress=bar --show-progress -O mkbootimg.tar.bz2
-# 	tar -xf mkbootimg.tar.bz2 --warning=no-timestamp
-# 	rm mkbootimg.tar.bz2
-# 	mv bootboot-master-mkbootimg/mkbootimg ./mkbootimg
-# 	rm -r bootboot-master-mkbootimg
-# 	cd mkbootimg
-# 	patch -u Makefile -i ../mkbootimg.patch -s
-# 	cd ..
-# 	if ! command -v fasm > /dev/null; then
-# 		wget https://flatassembler.net/fasm-1.73.30.tgz -q --progress=bar --show-progress -O fasm.tgz
-# 		tar -xf fasm.tgz
-# 		rm fasm.tgz
-# 		cd fasm
-# 		echo "(If a \"[sudo]\" prompt doesn't pop up, please ignore this) Please enter your password so that we can install fasm, an assembler that is required by the bootloader of this project."
-# 		install -m 755 fasm /usr/bin/fasm
-# 		cd ..
-# 		rm -r fasm
-# 	fi
-# 	cd mkbootimg
-# 	make &> /dev/null
-# 	cd ../..
-# fi
-# cp bootbootconfig isodir
-# cp bootboot.json isodir
-# cd isodir
-# ../util/mkbootimg/mkbootimg ./bootboot.json ./tfos.img
-# if [ $? != 0 ]
-# then
-# 	exit 1
-# fi
-# cd ..
-
