a393088e93f74d00b8e6df177a0317a49bca8123
Techflash <72118300+techflashYT@users.noreply.github.com>
2023-01-23 15:39:45 -0700

    Run with kvm

--- qemu.sh
+++ qemu.sh
@@ -1,1 +1,6 @@
-qemu-system-x86_64 -m 512M,slots=2,maxmem=1G -cpu core2duo -cdrom bin/TFOS_ISO.iso -display gtk -s $@
+#!/bin/sh
+if [ "$(id -u)" -ne 0 ]; then
+	printf "\033[1;33mPlease enter your sudo password to be able to run QEMU with KVM!\033[0m\r\n"
+	SUDO=sudo
+fi
+$SUDO qemu-system-x86_64 -m 512M,slots=2,maxmem=1G -cpu host --enable-kvm -cdrom bin/TFOS_ISO.iso -display gtk -s "$@"
