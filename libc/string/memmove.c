930d013cbc8c39421320ae3844a456764d30f91d
Techflash <72118300+techflashYT@users.noreply.github.com>
2023-04-08 12:29:35 -0700

    Make memmove look better.

--- libc/string/memmove.c
+++ libc/string/memmove.c
@@ -3,14 +3,17 @@
 #include <string.h>
 
 void *memmove(void *dest, const void *src, size_t n) {
-	unsigned char* dst = (unsigned char*) dest;
-	const unsigned char* srcTemp = (const unsigned char*) src;
+	uint8_t* dst = (uint8_t*) dest;
+	const uint8_t* srcTemp = (const uint8_t*) src;
 	if (dst < srcTemp) {
-		for (size_t i = 0; i < n; i++)
+		for (size_t i = 0; i < n; i++) {
 			dst[i] = srcTemp[i];
-	} else {
-		for (size_t i = n; i != 0; i--)
-			dst[i-1] = srcTemp[i-1];
+		}
+	}
+	else {
+		for (size_t i = n; i != 0; i--) {
+			dst[i - 1] = srcTemp[i - 1];
+		}
 	}
 	return dest;
 }
