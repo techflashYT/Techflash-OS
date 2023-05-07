ed5b42945ab2c389c3723f859184f70b00d649fe
Techflash <72118300+techflashYT@users.noreply.github.com>
2023-05-06 19:36:28 -0700

    Include bin2c implementation

--- util/symbols.sh
+++ util/symbols.sh
@@ -2,11 +2,11 @@
 # build symbols
 echo "Generating kernel symbols file..."
 if [ ! -f "bin/genSyms" ] || [ "$(find bin/genSyms -newer util/symToFile.c)" ]; then
-	gcc -Ofast -march=native -mtune=native -fsanitize=address,undefined symToFile.c -o bin/genSyms
+	gcc -Ofast -march=native -mtune=native -fsanitize=address,undefined util/symToFile.c -o bin/genSyms
 fi
 bin/genSyms
-if [ ! -f "bin/bin2c" ] || [ "$(find bin/bin2c -newer file2)" ]; then
-	gcc -Ofast -march=native -mtune=native -fsanitize=address,undefined symToFile.c -o bin/genSyms
+if [ ! -f "bin/bin2c" ] || [ "$(find bin/bin2c -newer util/bin2c)" ]; then
+	gcc -Ofast -march=native -mtune=native -fsanitize=address,undefined util/bin2c.c -o bin/bin2c
 fi
 bin/bin2c -o build/symbols.c build/symbols.bin
 gcc build/symbols.c -c -o build/symbols.o
