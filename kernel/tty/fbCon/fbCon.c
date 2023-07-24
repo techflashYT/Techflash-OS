397e77aaa2fdb3b5d0dde4a9d5bb9e06c1ff88b7
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-07-23 17:20:33 -0700

    kind of write to fb???

--- kernel/tty/fbCon/fbCon.c
+++ kernel/tty/fbCon/fbCon.c
@@ -68,32 +68,39 @@ void FB_DrawChar(const char ch, const uint_fast16_t xPos, const uint_fast16_t yP
 	(void)height;
 	UNPACK_WIDTH_HEIGHT(font.header.widthHeight, width, height);
 
-	for (uint_fast8_t i = 0; i != font.header.numGlyphs; i++) {
-		if (font.glyphs[i].glyphNum == ch) {
-			sprintf(str, "found matching glyph at index %d, glyph: 0x%02X\r\n", i, font.glyphs[i].glyphNum);
-			serial.writeString(SERIAL_PORT_COM1, str);
-			// find the specified glyph
-			uint8_t *bitfield = font.glyphs[i].glyphBitField;
-			int arrayIndex = 0;
-			
-			// iterate through each bit of the glyph
-			for (uint_fast8_t j = 0; j < height; j++) {
-				uint8_t row = bitfield[j];
-				for(uint_fast8_t k = 0; k < width; k++){
-					str[arrayIndex++] = (row & (1 << (7 - k))) ? '#' : ' ';
-				}
-				str[arrayIndex++] = '\r';
-				str[arrayIndex++] = '\n';                
-			}
-			
-			// Null-terminating the array to be safe when printing 
-			str[arrayIndex] = '\0';
 
-			serial.writeString(SERIAL_PORT_COM1, str);
-			
+	uint_fast64_t fbLine = TTY_CursorY * height;
+	uint_fast64_t fbIndex = (TTY_CursorX * (width + charSpacing));
+	uint_fast8_t i;
+	for (i = 0; i != font.header.numGlyphs; i++) {
+		if (font.glyphs[i].glyphNum == ch) {
 			break;
 		}
 	}
+
+	sprintf(str, "found matching glyph at index %d, glyph: 0x%02X\r\n", i, font.glyphs[i].glyphNum);
+	serial.writeString(SERIAL_PORT_COM1, str);
+	// find the specified glyph
+	uint8_t *bitfield = font.glyphs[i].glyphBitField;
+	int arrayIndex = 0;
+	
+	// iterate through each bit of the glyph
+	for (uint_fast8_t j = 0; j < height; j++) {
+		uint8_t row = bitfield[j];
+		for(uint_fast8_t k = 0; k < width; k++){
+			str[arrayIndex++] = (row & (1 << (7 - k))) ? '#' : ' ';
+			((uint32_t *)fb->address)[(fbIndex++) + (fbLine * fb->width)] = (row & (1 << (7 - k))) ? TTY_Color : TTY_TextBackground;
+		}
+		str[arrayIndex++] = '\r';
+		str[arrayIndex++] = '\n';
+		fbLine++;
+		fbIndex = 0;
+	}
+	
+	// Null-terminating the array to be safe when printing 
+	str[arrayIndex] = '\0';
+
+	serial.writeString(SERIAL_PORT_COM1, str);
 
 
 	TTY_CursorX++;
