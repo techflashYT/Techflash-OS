33fe1eb3e918a989df7034a813151d20b4292f71
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-07-25 12:46:03 -0700

    Fix weirdness in puts

--- libc/stdio/puts.c
+++ libc/stdio/puts.c
@@ -1,28 +1,13 @@
 // NOTE: Yes, this is indeed the same file between both the internal libc and the userspace wrappers, I just symlinked it across.
 #include <stdio.h>
 #include <stdint.h>
-// int len = 0;
-char buffer[16] = {0};
 int puts(const char *s) {
 	/*
 		Each iteration of this while loop prints a single character.
 	*/
-	// len = 0;
-	if ((*s) == 0) {
-		putchar('w');
-		putchar('e');
-		putchar(' ');
-		putchar('d');
-		putchar('e');
-		putchar('d');
-		putchar(' ');
-		putchar(' ');
-		return 0;
-	}
 	while (*s) {
 		putchar(*s);
 		s++;
-		// len++;
 	}
 	return 0;
 }
