b1961f1009fb58f616f3a1139f9761916e62a4c2
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-07-29 15:50:40 -0700

    try harder to get limine SMP to work

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,19 +5,24 @@
 #include <kernel/tty.h>
 #include <kernel/arch.h>
 
-struct limine_smp_request smpRequest = {
+volatile struct limine_smp_request smpRequest = {
 	.id = LIMINE_SMP_REQUEST,
 	.revision = 0,
-	.flags = 1
+	.flags = 0
 };
 
 void main() {
 	TTY_SetWriteFunc(COM_LogWrapper);
+	if (smpRequest.response == NULL) {
+		puts("Limine didn't give us a valid SMP response...\r\n");
+	}
 	printf(
-		"SMP Request info:\r\n"
+		"SMP Response Info:\r\n"
+		"  - Response revision: %lu\r\n"
 		"  - Number of CPUs: %lu\r\n"
-		"  - Bootstrap Processor Local APIC ID: %lu\r\n"
+		"  - Bootstrap Processor Local APIC ID: %u\r\n"
 		"  - X2APIC: %s\r\n",
+		smpRequest.response->revision,
 		smpRequest.response->cpu_count,
 		smpRequest.response->bsp_lapic_id,
 		(smpRequest.response->flags & 1) ? "Enabled" : "Disabled"
