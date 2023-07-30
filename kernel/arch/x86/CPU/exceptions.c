e5e42aec447b187b681c291c810342fdba50b25a
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-07-30 00:51:18 -0700

    exceptions

--- kernel/arch/x86/CPU/exceptions.c
+++ kernel/arch/x86/CPU/exceptions.c
@@ -1,23 +1,24 @@
 #include <kernel/panic.h>
+#include <kernel/arch/x86/ISR.h>
 #include <stdio.h>
 // 0
-void zeroDivHandler(registers_t *regs) {panic("(#DE) Integer Division by Zero", regs);}
+static void zeroDivHandler(registers_t *regs) {panic("(#DE) Integer Division by Zero", regs);}
 // 1, 3
-void debugHandler(__attribute__ ((unused)) registers_t *regs) {/* TODO: Start a kernel debugger here */printf("\r\nWould start a kernel debugger here but it's not implemented!\r\n");}
+static void debugHandler(__attribute__ ((unused)) registers_t *regs) {/* TODO: Start a kernel debugger here */printf("\r\nWould start a kernel debugger here but it's not implemented!\r\n");}
 // 2
-void NMIHandler(registers_t *regs) {panic("Non-Maskable Interrupt", regs);}
+static void NMIHandler(registers_t *regs) {panic("Non-Maskable Interrupt", regs);}
 // 4
-void overflowHandler(registers_t *regs) {panic("(#OF) Overflow", regs);}
+static void overflowHandler(registers_t *regs) {panic("(#OF) Overflow", regs);}
 // 5
-void boundRangeExceededHandler(registers_t *regs) {panic("(#BR) Bound Range Exceeded", regs);}
+static void boundRangeExceededHandler(registers_t *regs) {panic("(#BR) Bound Range Exceeded", regs);}
 // 6
-void invalidOpcodeHandler(registers_t *regs) {panic("(#UD) Invalid Opcode (likely a bad jump)", regs);}
+static void invalidOpcodeHandler(registers_t *regs) {panic("(#UD) Invalid Opcode (likely a bad jump)", regs);}
 /*
 	7 is "Device not available", which is used when an FPU instruction is executed but there is no FPU present.
 	This is simply impossible as the bootloader that this OS uses will not run on a CPU that is that old.
 */
 // 8
-void doubleFaultHandler(registers_t *regs) {panic("(#DF) Double Fault", regs);}
+static void doubleFaultHandler(registers_t *regs) {panic("(#DF) Double Fault", regs);}
 /*
 	9 is "Coprocessor Segment Overrun", this is when the FPU would perform segment checking and it detected an overrun.
 	This is no longer in effect as the modern internal FPU just raises a #GP for segment overruns.
@@ -26,30 +27,63 @@ void doubleFaultHandler(registers_t *regs) {panic("(#DF) Double Fault", regs);}
 	10 is "Invalid TSS", since we don't use TSS yet, this is unimplemented.
 */
 // 11 (0xB)
-void segmentNotPresetHandler(registers_t *regs) {panic("(#NP) Segment Not Present", regs);}
+static void segmentNotPresetHandler(registers_t *regs) {panic("(#NP) Segment Not Present", regs);}
 // 12 (0xC)
-void stackSegmentFaultHandler(registers_t *regs) {panic("(#SS) Stack Segment Fault", regs);}
+static void stackSegmentFaultHandler(registers_t *regs) {panic("(#SS) Stack Segment Fault", regs);}
 // 13 (0xD)
-void generalProtectionFaultHandler(registers_t *regs) {panic("(#GP) General Protection Fault", regs);}
+static void generalProtectionFaultHandler(registers_t *regs) {panic("(#GP) General Protection Fault", regs);}
 // 14 (0xE)
-void pageFaultHandler(registers_t *regs) {panic("(#PF) Page Fault", regs);}
+static void pageFaultHandler(registers_t *regs) {panic("(#PF) Page Fault", regs);}
 // 15 (0xF), 22-27 (0x16-0x1B), 31 (0x1F)
-void reservedHandler(registers_t *regs) {panic("Reserved Exception.... wtf????????", regs);}
+static void reservedHandler(registers_t *regs) {panic("Reserved Exception.... wtf????????", regs);}
 // 16 (0x10)
-void FPUExceptionHandler(registers_t *regs) {panic("(#MF) Floating Point Exception", regs);}
+static void FPUExceptionHandler(registers_t *regs) {panic("(#MF) Floating Point Exception", regs);}
 // 17 (0x11)
-void alignmentCheckExceptionHandler(registers_t *regs) {panic("(#AC) Alignment Check Exception", regs);}
+static void alignmentCheckExceptionHandler(registers_t *regs) {panic("(#AC) Alignment Check Exception", regs);}
 // 18 (0x12)
-void machineCheckExceptionHandler(registers_t *regs) {panic("(#MC) Machine Check Exception", regs);}
+static void machineCheckExceptionHandler(registers_t *regs) {panic("(#MC) Machine Check Exception", regs);}
 // 19 (0x13)
-void SSEExceptionHandler(registers_t *regs) {panic("(#XF) SSE Exception", regs);}
+static void SSEExceptionHandler(registers_t *regs) {panic("(#XF) SSE Exception", regs);}
 // 20 (0x14)
-void virtualizationExceptionHandler(registers_t *regs) {panic("(#VE) Virtualization Exception", regs);}
+static void virtualizationExceptionHandler(registers_t *regs) {panic("(#VE) Virtualization Exception", regs);}
 // 21 (0x15)
-void controlProtectionExceptionHandler(registers_t *regs) {panic("(#CP) Control Protection Exception", regs);}
+static void controlProtectionExceptionHandler(registers_t *regs) {panic("(#CP) Control Protection Exception", regs);}
 // 28 (0x1C)
-void hypervisorInjectionExceptionHandler(registers_t *regs) {panic("(#HV) Hypervisor Injection Exception", regs);}
+static void hypervisorInjectionExceptionHandler(registers_t *regs) {panic("(#HV) Hypervisor Injection Exception", regs);}
 // 29 (0x1D)
-void VMMCommunicationExceptionHandler(registers_t *regs) {panic("(#VC) VMM Communication Exception", regs);}
+static void VMMCommunicationExceptionHandler(registers_t *regs) {panic("(#VC) VMM Communication Exception", regs);}
 // 30 (0x1E)
-void securityExceptionHandler(registers_t *regs) {panic("(#SX) Security Exception", regs);}
+static void securityExceptionHandler(registers_t *regs) {panic("(#SX) Security Exception", regs);}
+
+
+void x86_Exceptions_Init() {
+	x86_ISR_RegisterHandler(0,  &zeroDivHandler);
+	x86_ISR_RegisterHandler(1,  &debugHandler);
+	x86_ISR_RegisterHandler(2,  &NMIHandler);
+	x86_ISR_RegisterHandler(3,  &debugHandler);
+	x86_ISR_RegisterHandler(4,  &overflowHandler);
+	x86_ISR_RegisterHandler(5,  &boundRangeExceededHandler);
+	x86_ISR_RegisterHandler(6,  &invalidOpcodeHandler);
+	x86_ISR_RegisterHandler(8,  &doubleFaultHandler);
+	x86_ISR_RegisterHandler(11, &segmentNotPresetHandler);
+	x86_ISR_RegisterHandler(12, &stackSegmentFaultHandler);
+	x86_ISR_RegisterHandler(13, &generalProtectionFaultHandler);
+	x86_ISR_RegisterHandler(14, &pageFaultHandler);
+	x86_ISR_RegisterHandler(15, &reservedHandler);
+	x86_ISR_RegisterHandler(16, &FPUExceptionHandler);
+	x86_ISR_RegisterHandler(17, &alignmentCheckExceptionHandler);
+	x86_ISR_RegisterHandler(18, &machineCheckExceptionHandler);
+	x86_ISR_RegisterHandler(19, &SSEExceptionHandler);
+	x86_ISR_RegisterHandler(20, &virtualizationExceptionHandler);
+	x86_ISR_RegisterHandler(21, &controlProtectionExceptionHandler);
+	x86_ISR_RegisterHandler(22, &reservedHandler);
+	x86_ISR_RegisterHandler(23, &reservedHandler);
+	x86_ISR_RegisterHandler(24, &reservedHandler);
+	x86_ISR_RegisterHandler(25, &reservedHandler);
+	x86_ISR_RegisterHandler(26, &reservedHandler);
+	x86_ISR_RegisterHandler(27, &reservedHandler);
+	x86_ISR_RegisterHandler(28, &hypervisorInjectionExceptionHandler);
+	x86_ISR_RegisterHandler(29, &VMMCommunicationExceptionHandler);
+	x86_ISR_RegisterHandler(30, &securityExceptionHandler);
+	x86_ISR_RegisterHandler(31, &reservedHandler);
+}
