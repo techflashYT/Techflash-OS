636cd18769c737ff064232b8a3bb32ff675627aa
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-08-02 08:13:23 -0700

    Add hex labels

--- kernel/arch/x86_64/CPU/printRegs.c
+++ kernel/arch/x86_64/CPU/printRegs.c
@@ -6,21 +6,21 @@ void ARCH_PrintRegs(registers_t *regs) {
 	asm volatile("mov %%cr2, %0" : "=r"(cr2));
 
 	printf(
-		"  RAX:   %016lX  RBX:   %016lX  RCX:   %016lX\r\n"
-		"  RDX:   %016lX  RDI:   %016lX  RSI:   %016lX\r\n"
-		"  R8:    %016lX  R9:    %016lX  R10:   %016lX\r\n"
-		"  R11:   %016lX  R12:   %016lX  R13:   %016lX\r\n"
-		"  R14:   %016lX  R15:   %016lX\r\n"
-		"  Int:   %016lX  err:   %016lX  CR2:   %016lX\r\n"
-		"  RIP:   %016lX  RBP:   %016lX  RSP:   %016lX\r\n"
-		"  CS:    %016lX  SS:    %016lX  flags: %016lX\r\n",
+		"  RAX:   0x%016lX  RBX:   0x%016lX  RCX:   0x%016lX\r\n"
+		"  RDX:   0x%016lX  RDI:   0x%016lX  RSI:   0x%016lX\r\n"
+		"  R8:    0x%016lX  R9:    0x%016lX  R10:   0x%016lX\r\n"
+		"  R11:   0x%016lX  R12:   0x%016lX  R13:   0x%016lX\r\n"
+		"  R14:   0x%016lX  R15:   0x%016lX\r\n"
+		"  Int:   0x%016lX  err:   0x%016lX  CR2:   0x%016lX\r\n"
+		"  RIP:   0x%016lX  RBP:   0x%016lX  RSP:   0x%016lX\r\n"
+		"  CS:    0x%016lX  SS:    0x%016lX  flags: 0x%016lX\r\n",
 		regs->rax, regs->rbx, regs->rcx,
 		regs->rdx, regs->rdi, regs->rsi,
 		regs->r8,  regs->r9,  regs->r10,
 		regs->r11, regs->r12, regs->r13,
 		regs->r14, regs->r15,
 		regs->intNo, regs->errCode, cr2,
-		regs->rip, regs->rbp, regs->userRsp,
+		regs->rip, regs->rbp, regs->rsp,
 		regs->cs, regs->ss, regs->rflags
 	);
 }
