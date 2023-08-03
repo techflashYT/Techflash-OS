8bc3e2a05e42614e2e08751ab6cd54fb8531c67a
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-08-03 16:16:37 -0700

    Fix interrupts

--- include/kernel/arch/x86/registers.h
+++ include/kernel/arch/x86/registers.h
@@ -4,4 +4,4 @@ typedef struct {
 	uint64_t r8, r9, r10, r11, r12, r13, r14, r15;
 	uint64_t intNo, errCode;
 	uint64_t rip, cs, rflags, rsp, ss;
-} registers_t;
+} __attribute__((packed)) registers_t;
