a4956cc27dfb16e2a9962cf1d2f887984dfeb8cd
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-08-03 16:17:08 -0700

    Begin trying to print memory map

--- kernel/mm/pmm.c
+++ kernel/mm/pmm.c
@@ -5,6 +5,7 @@
 #include <kernel/panic.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 MODULE("PMM");
 extern memmap_t *LM_ParseMemmap();
 static memmap_t *BOOT_ParseMemmap() {
@@ -17,6 +18,33 @@ static memmap_t *BOOT_ParseMemmap() {
 	panic("No memory map detected", &regs);
 	return NULL;
 }
+
+static void PMM_CalcSizeStr(char *sizeStr, size_t size) {
+	double sizeFlt = (double)size;
+	uint_fast8_t i;
+	char types[] = {
+		'B',
+		'K',
+		'M',
+		'G',
+		'T',
+		'P'
+	};
+	for (i = 0; i != 8; i++) {
+		if ((size % 1024) == 0) {
+			size /= 1024;
+			sizeFlt /= 1024;
+			continue;
+		}
+		if ((size / 1024) >= 10) {
+			size /= 1024;
+			sizeFlt /= 1024;
+			continue;
+		}
+	}
+	sprintf(sizeStr, "%.02g%c", sizeFlt, types[i]);
+}
+
 void PMM_Init() {
 	log(MODNAME, "PMM Initializing", LOGLEVEL_INFO);
 	log(MODNAME, "Getting memory map from bootloader...", LOGLEVEL_DEBUG);
@@ -27,4 +55,29 @@ void PMM_Init() {
 
 	sprintf(str, "Got memory map with %d entries at %p", memmap->numEntries, memmap);
 	log(MODNAME, str, LOGLEVEL_DEBUG);
+
+	for (uint_fast8_t i = 0; i != memmap->numEntries; i++) {
+		/* TODO: Make size & type strings with the following logic:
+		- `(size % 1024) == 0`
+			- YES, Cleanly divisible by 1024, just divide by 1024 and repeat the loop
+			- NO, Not cleanly divisible by 1024, continue below
+		- `(size / 1024) >= 10`
+			- YES, dividing by 1024 gives a number greater than 10, just divide by 1024 and repeat the loop, keeping track of the decimal portion of the number.
+			- NO, dividing by 1024 gives a number less than 10, continue below
+		- Is the number a clean integer, with no remaining decimal places?  (e.g. 10)
+			- YES, treat it as a basic integer, don't include the decimal portion in the string.
+			- NO, it's a decimal number, include the decimal portion in the string
+		- By this point, the current state of the number should be: Greater than 10, less than 1024, either a decimal number, or standard integer.
+		- Calculate the size, adding the suffix of `B` (bytes), `K` (KB), `M` (MB), `G`, (GB), `T`, (TB), `P`, (PB). 
+		*/
+		uint64_t size = memmap->entries[i].size;
+		char sizeStr[16];
+		char *typeStr = "yourMom";
+
+		// Make size & type strings
+		PMM_CalcSizeStr(sizeStr, size);
+
+		sprintf(str, "Entry %d: %p - %p; %s; Type: %s", i, memmap->entries[i].start, memmap->entries[i].start + memmap->entries[i].size, sizeStr, typeStr);
+		log(MODNAME, str, LOGLEVEL_DEBUG);
+	}
 }
