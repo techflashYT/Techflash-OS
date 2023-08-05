b87ad0a11ccf30ca7df22e1cc3cbede65680a5ef
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-08-05 11:35:15 -0700

    comment out unused defines

--- kernel/arch/x86/PIC.c
+++ kernel/arch/x86/PIC.c
@@ -8,9 +8,9 @@ MODULE("X86PIC");
 #define PIC1		 0x20		/* IO base address for master PIC */
 #define PIC2		 0xA0		/* IO base address for slave PIC */
 #define PIC1_COMMAND PIC1
-#define PIC1_DATA	 (PIC1+1)
+// #define PIC1_DATA	 (PIC1+1)
 #define PIC2_COMMAND PIC2
-#define PIC2_DATA	 (PIC2+1)
+// #define PIC2_DATA	 (PIC2+1)
 #define PIC_EOI		 0x20
 static isr_t interruptHandlers[256] = {0};
 static uint64_t lastInterruptNumber = 0;
